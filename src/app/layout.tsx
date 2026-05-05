7a4b0e2692c1083e0d8352ff468ae67401a3613f
wenbo <9009wwb@gmail.com>
2026-05-05 14:23:39 +0800

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,6 @@
+import '../styles/global.css';
+import '@rainbow-me/rainbowkit/styles.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -9,7 +9,7 @@ import {
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
-  appName: 'RainbowKit App',
+  appName: 'ForgeNFT',
   projectId: 'YOUR_PROJECT_ID',
   chains: [
     mainnet,

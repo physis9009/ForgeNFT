06e56a28dfb415919b7b56ad8fcaeefb7481e6da
wenbo <9009wwb@gmail.com>
2026-05-21 20:36:14 +0800

    Try to resolve Vercel deployment issues

--- src/app/[locale]/(mint)/layout.tsx
+++ src/app/[locale]/(mint)/layout.tsx
@@ -6,15 +6,16 @@ import Footer from "@/src/components/footer";
 
 export default async function Layout({children, params}: {
     children: ReactNode;
-    params: Promise<{locale: Locale}>;
+    params: Promise<{locale: string}>;
 }) {
     const {locale} = await params;
+    const safeLocale = locale as Locale;
 
     return (
         <div className="min-h-screen flex flex-col"> 
             <nav className="grid grid-cols-5 justify-around justify-items-center fixed top-0 left-0 right-0 backdrop-blur-md shadow-lg items-center">
-                <MintLink locale={locale} />
-                <GalleryLink locale={locale} />
+                <MintLink locale={safeLocale} />
+                <GalleryLink locale={safeLocale} />
                 <span className="inline-block font-extrabold text-2xl text-grn">ForgeNFT</span>
                 <div className="justify-self-end-safe col-span-2"><ConnectButton /></div>
             </nav>
